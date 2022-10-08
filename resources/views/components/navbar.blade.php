07fcc0f1e62f92a52c2c5371b78ba5db23f324f7
revamp sobatbangun <revamp.sobatbangun@gmail.com>
2022-10-08 12:53:06 +0700

    minopr update

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -35,7 +35,7 @@
         {{-- <li class="nav-item">
             <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
                 <i class="fas fa-th-large"></i>
-            </a>
+            </a>git 
         </li> --}}
     </ul>
   </nav>

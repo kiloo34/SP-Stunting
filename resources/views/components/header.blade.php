fc908818bb6617eeeabd1c4bd123e4164e21f3f3
revamp sobatbangun <revamp.sobatbangun@gmail.com>
2022-10-08 12:42:18 +0700

    feature redirect to view dashboard every user

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,43 @@
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Dashboard</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item">
+                        @if (auth()->user()->role->name == 'Penyuluh')
+                        <a href="{{ route('penyuluh.dashboard') }}">    
+                        @elseif (auth()->user()->role->name == 'Bidan')
+                        <a href="{{ route('bidan.dashboard') }}">
+                        @elseif (auth()->user()->role->name == 'PKK')
+                        <a href="{{ route('pkk.dashboard') }}">
+                        @elseif (auth()->user()->role->name == 'Kader')
+                        <a href="{{ route('kader.dashboard') }}">
+                        @else
+                        <a href="#">
+                        @endif
+                            Home
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item active">
+                        @if (auth()->user()->role->name == 'Penyuluh')
+                        <a href="{{ route('penyuluh.dashboard') }}">    
+                        @elseif (auth()->user()->role->name == 'Bidan')
+                        <a href="{{ route('bidan.dashboard') }}">
+                        @elseif (auth()->user()->role->name == 'PKK')
+                        <a href="{{ route('pkk.dashboard') }}">
+                        @elseif (auth()->user()->role->name == 'Kader')
+                        <a href="{{ route('kader.dashboard') }}">
+                        @else
+                        <a href="#">
+                        @endif
+                            {{ucfirst($title)}}
+                        </a>
+                    </li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>

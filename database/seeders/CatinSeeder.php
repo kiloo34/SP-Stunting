e898d10e65088e4abde4b57339a9f6997b25ed5d
robi leksono <robileksono@gmail.com>
2022-10-09 10:59:04 +0700

    add active statuses in catin data

--- database/seeders/CatinSeeder.php
+++ database/seeders/CatinSeeder.php
@@ -18,33 +18,158 @@ class CatinSeeder extends Seeder
         \DB::table('catin')->truncate();
         \DB::table('catin')->insert([
             [
-                'name' => 'Siti',
+                'name' => 'Munifah',
                 'nik' => '2945824929492941',
                 'no_hp' => '081233334444',
+                'age' => '32',
                 'address' => 'ini alamat dump',
-                'village_id' => 1 
+                'village_id' => 1 ,
+                'status_id' => 1
             ],
             [
-                'name' => 'Icha',
+                'name' => 'Wardatul Hoiriyah',
                 'nik' => '2945824929492941',
                 'no_hp' => '081233334444',
+                'age' => '20',
                 'address' => 'ini alamat dump',
-                'village_id' => 2
+                'village_id' => 2,
+                'status_id' => 1
             ],
             [
-                'name' => 'Kirana',
+                'name' => 'Dewi Nur Aini',
                 'nik' => '2945824929492941',
                 'no_hp' => '081233334444',
+                'age' => '23',
                 'address' => 'ini alamat dump',
-                'village_id' => 3 
+                'village_id' => 3 ,
+                'status_id' => 1
             ],
             [
-                'name' => 'Dela',
+                'name' => 'Ila Wardiyah',
                 'nik' => '2945824929492941',
                 'no_hp' => '081233334444',
+                'age' => '24',
                 'address' => 'ini alamat dump',
-                'village_id' => 4 
-            ]
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'RIKA',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '19',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'Misnaya',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '19',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'Halima',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '22',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'Siti Marifah',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '20',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'Koyim',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '17',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'Wiwin Husniah',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '21',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'Siti Marifah',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '20',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'Sofianatul Hasanah',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '19',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'Laily Agustin',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '22',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'Ifatun Nisak',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '24',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'Refika Amfelia Febiola',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '22',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'Siti Maimunah',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '28',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
+            [
+                'name' => 'Yulia Tri Susanti',
+                'nik' => '2945824929492941',
+                'no_hp' => '081233334444',
+                'age' => '26',
+                'address' => 'ini alamat dump',
+                'village_id' => 4 ,
+                'status_id' => 1
+            ],
         ]);
     }
 }

f4f2a4f154303fc9d9e74c40c06ae2d7d936d94f
robi leksono <robileksono@gmail.com>
2022-10-09 19:11:46 +0700

    update url dashboard

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -8,34 +8,67 @@
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item">
                         @if (auth()->user()->role->name == 'Penyuluh')
-                        <a href="{{ route('penyuluh.dashboard') }}">    
+                        <a href="{{ route('penyuluh.dashboard.index') }}">    
                         @elseif (auth()->user()->role->name == 'Bidan')
-                        <a href="{{ route('bidan.dashboard') }}">
+                        <a href="{{ route('bidan.dashboard.index') }}">
                         @elseif (auth()->user()->role->name == 'PKK')
-                        <a href="{{ route('pkk.dashboard') }}">
+                        <a href="{{ route('pkk.dashboard.index') }}">
                         @elseif (auth()->user()->role->name == 'Kader')
-                        <a href="{{ route('kader.dashboard') }}">
+                        <a href="{{ route('kader.dashboard.index') }}">
                         @else
                         <a href="#">
                         @endif
                             Home
                         </a>
                     </li>
-                    <li class="breadcrumb-item active">
+                    @if ($subtitle != '')
+                    <li class="breadcrumb-item">
                         @if (auth()->user()->role->name == 'Penyuluh')
-                        <a href="{{ route('penyuluh.dashboard') }}">    
+                        <a href="{{ route('penyuluh.'.$title.'.index') }}">    
                         @elseif (auth()->user()->role->name == 'Bidan')
-                        <a href="{{ route('bidan.dashboard') }}">
+                        <a href="{{ route('bidan.'.$title.'.index') }}">
                         @elseif (auth()->user()->role->name == 'PKK')
-                        <a href="{{ route('pkk.dashboard') }}">
+                        <a href="{{ route('pkk.'.$title.'.index') }}">
                         @elseif (auth()->user()->role->name == 'Kader')
-                        <a href="{{ route('kader.dashboard') }}">
+                        <a href="{{ route('kader.'.$title.'.index') }}">
                         @else
                         <a href="#">
                         @endif
                             {{ucfirst($title)}}
                         </a>
                     </li>
+                    <li class="breadcrumb-item active">
+                        @if (auth()->user()->role->name == 'Penyuluh')
+                        <a href="{{ route('penyuluh.'.$title.'.'.$subtitle) }}">    
+                        @elseif (auth()->user()->role->name == 'Bidan')
+                        <a href="{{ route('bidan.'.$title.'.'.$subtitle) }}">
+                        @elseif (auth()->user()->role->name == 'PKK')
+                        <a href="{{ route('pkk.'.$title.'.'.$subtitle) }}">
+                        @elseif (auth()->user()->role->name == 'Kader')
+                        <a href="{{ route('kader.'.$title.'.'.$subtitle) }}">
+                        @else
+                        <a href="#">
+                        @endif
+                            {{ucfirst($subtitle)}}
+                        </a>
+                    </li>
+                    @else
+                    <li class="breadcrumb-item active">
+                        @if (auth()->user()->role->name == 'Penyuluh')
+                        <a href="{{ route('penyuluh.'.$title.'.index') }}">    
+                        @elseif (auth()->user()->role->name == 'Bidan')
+                        <a href="{{ route('bidan.'.$title.'.index') }}">
+                        @elseif (auth()->user()->role->name == 'PKK')
+                        <a href="{{ route('pkk.'.$title.'.index') }}">
+                        @elseif (auth()->user()->role->name == 'Kader')
+                        <a href="{{ route('kader.'.$title.'.index') }}">
+                        @else
+                        <a href="#">
+                        @endif
+                            {{ucfirst($title)}}
+                        </a>
+                    </li>
+                    @endif
                 </ol>
             </div><!-- /.col -->
         </div><!-- /.row -->

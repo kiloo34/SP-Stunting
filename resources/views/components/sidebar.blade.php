010967084c4f02991e56fe79e8a111b6c442403f
robi leksono <robileksono@gmail.com>
2022-10-13 00:01:08 +0700

    add configuration for user tim

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -55,6 +55,12 @@
                 <li class="nav-item">
                     <a href="{{ route('penyuluh.catin.index') }}" class="nav-link {{ $active == 'catin' ? 'active' : '' }}">
                         <i class="nav-icon fas fa-users"></i>
+                        <p>{{__('Pengguna')}}</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('penyuluh.catin.index') }}" class="nav-link {{ $active == 'catin' ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-users"></i>
                         <p>{{__('Catin')}}</p>
                     </a>
                 </li>

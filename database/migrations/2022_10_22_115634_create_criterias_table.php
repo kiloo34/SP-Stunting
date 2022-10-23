89fef1a9a09aeefc10fe744ea88b2fc9eb44e4a2
revamp sobatbangun <revamp.sobatbangun@gmail.com>
2022-10-23 19:09:38 +0700

    update string -> int

--- database/migrations/2022_10_22_115634_create_criterias_table.php
+++ database/migrations/2022_10_22_115634_create_criterias_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('criterias', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('value');
+            $table->integer('value');
             $table->timestamps();
         });
     }
